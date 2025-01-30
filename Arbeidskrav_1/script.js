4ae2f6afc94889ecc77b0e57b55425b6c9be82e5
ahmedna04 <ahmedna@hiof.no>
2025-01-30 13:24:45 +0100

    har lagt til en kommentar som forklarer litt hva koden gjør, har begynt å bruke join i script js

--- Arbeidskrav_1/script.js
+++ Arbeidskrav_1/script.js
@@ -5,7 +5,10 @@ const liste = document.getElementById("ressurs-liste");
 const knapper = document.querySelectorAll("nav button");
 
 function ressurser(kategori) {
-
     const valgtKategori = resource.find(res => res.kategori === kategori);
-    tittel.textContent = valgtKategori.kategori
+    tittel.textContent = valgtKategori.kategori;
+    beskrivelse.textContent = valgtKategori.text;
+    liste.innerHTML = valgtKategori.sources.map(kilde => 
+        `<li><a href="${kilde.url}">${kilde.title}</a></li>`
+    ).join("");
 }
