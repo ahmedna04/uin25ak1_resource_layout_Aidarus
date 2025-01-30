bc69e58a1a82d2e7b0dc70c38950601f47f25d09
ahmedna04 <ahmedna@hiof.no>
2025-01-30 12:07:56 +0100

    Begynt å skriv mer kode i script.js

--- Arbeidskrav_1/script.js
+++ Arbeidskrav_1/script.js
@@ -4,3 +4,8 @@ const beskrivelse = document.getElementById("kategori-beskrivelse");
 const liste = document.getElementById("ressurs-liste");
 const knapper = document.querySelectorAll("nav button");
 
+function ressurser(kategori) {
+
+    const valgtKategori = resource.find(res => res.kategori === kategori);
+    tittel.textContent = valgtKategori.kategori
+}
