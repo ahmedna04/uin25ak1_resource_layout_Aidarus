d391af2f5a75bd009e8f1881ceb8149bffe7edf1
ahmedna04 <ahmedna@hiof.no>
2025-01-30 11:30:14 +0100

    Ryddet litt i HTML strukturen, har begynt å kode litt i script.js

---
+++ Arbeidskrav_1/script.js
@@ -0,0 +1,6 @@
+
+const tittel = document.getElementById("kategori-tittel");
+const beskrivelse = document.getElementById("kategori-beskrivelse");
+const liste = document.getElementById("ressurs-liste");
+const knapper = document.querySelectorAll("nav button");
+
